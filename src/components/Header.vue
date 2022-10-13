61d05b56fde14bce6c5c57a4722195eb3e7cfdca
maple <me@spce.moe>
2022-10-13 21:16:57 +0200

    back 2 basics

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="navigation">
+        <header>
+            <div class="mx-auto flex">
+
+                <div class="p-8 mx-auto">
+                    <a href="#" class="text-red-500"><img src="../assets/logo.png">Mediarr UI</a>
+                </div>
+
+            </div>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+img {
+    max-width: 65px;
+    max-height: 65px;
+}
+</style>
